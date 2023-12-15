f838b2b0406add8a5c64c6b0253505b0c765f9e6
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2023-12-15 17:08:14 +0200

    frontend link

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const userRoutes = require("./routes/userRoutes");
-const feedingRoutes=require("./routes/feedingRoutes");
+const feedingRoutes = require("./routes/feedingRoutes");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(cors({ origin: "https://genuine-arithmetic-b0c8ef.netlify.app/" }));
 app.use(express.json());
 
 app.use("/api/v1/", userRoutes);
-app.use("/api/v1/animals",feedingRoutes);
+app.use("/api/v1/animals", feedingRoutes);
 
 module.exports = app;
