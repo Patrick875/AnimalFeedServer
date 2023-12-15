1fdf67a9a78189962df84a781479d24c2f627ab6
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2023-12-15 09:35:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const userRoutes = require("./routes/userRoutes");
+const feedingRoutes=require("./routes/feedingRoutes");
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/", userRoutes);
+app.use("/api/v1/animals",feedingRoutes);
 
 module.exports = app;
